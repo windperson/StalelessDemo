aa8a921b7e2245fe4dd20d8d32444e2f9599d842
Chen Yu Pao <windperson@gmail.com>
2019-04-15 15:19:23 +0800

    Add project files.

---
+++ StalelessDemo/FsmOnOff.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Stateless;
+
+namespace StalelessDemo
+{
+    class FsmOnOff
+    {
+        #region State & Trigger Declaration
+
+        public const string ON_State = "ON";
+        public const string OFF_State = "OFF";
+
+        public const char SWITCH_ON_Trigger = '=';
+        public const char SWITCH_OFF_Trigger = '-';
+
+        #endregion
+
+        private readonly StateMachine<string, char> _stateMachine;
+
+        private bool _isOnReentry;
+
+        public FsmOnOff(string initialState)
+        {
+            _stateMachine = new StateMachine<string, char>(initialState);
+
+            ConfigStateMachine();
+        }
+
+        private void ConfigStateMachine()
+        {
+            _stateMachine.Configure(ON_State)
+                .Permit(SWITCH_OFF_Trigger, OFF_State)
+                .InternalTransition(SWITCH_ON_Trigger, transition => { _isOnReentry = true; })
+                .OnExit(() => { _isOnReentry = false; })
+                ;
+
+            _stateMachine.Configure(OFF_State)
+                .Permit(SWITCH_ON_Trigger, ON_State)
+                .InternalTransition(SWITCH_OFF_Trigger, transition => { _isOnReentry = true; })
+                .OnExit(() => { _isOnReentry = false; })
+                ;
+        }
+
+        public string DisplayCurrentState()
+        {
+            if (_isOnReentry)
+            {
+                return $"\n[Switch is already in state [{_stateMachine.State}]!";
+            }
+
+            return $"\n[Switch is in state {_stateMachine.State}]";
+        }
+
+        public void Input(char inputKey)
+        {
+            //NOTE: should use if(_stateMachine.CanFire(inputKey)) to check if it is valid input.
+
+            _stateMachine.Fire(inputKey);
+        }
+
+    }
+}
---
+++ StalelessDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace StalelessDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var onOffSwitch = new FsmOnOff(FsmOnOff.OFF_State);
+
+            Console.WriteLine("Press '-' , '=' key to toggle the switch.  'x' key will exit the program.");
+
+            while (true)
+            {
+                Console.WriteLine(onOffSwitch.DisplayCurrentState());
+
+                var pressedKey = Console.ReadKey().KeyChar;
+
+                if (pressedKey == 'x') { break; }
+
+                try
+                {
+                    onOffSwitch.Input(pressedKey);
+                }
+                catch (InvalidOperationException ex)
+                {
+                    Console.WriteLine(ex);
+                    Console.WriteLine("\nPress Enter to continue");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
